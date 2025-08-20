b828dd5544288332e30083c54f1d3cd982fe4363
mathew-onwuka <mathew.onwuka@gmail.com>
2025-08-20 09:48:48 +0100

    first version

---
+++ jquery.js
@@ -0,0 +1,8 @@
+$('.p0').parallax({imageSrc:"images2/parallax0.png"});
+$('.p1').parallax({imageSrc:"images2/p1.png"});
+$('.p2').parallax({imageSrc:"images2/p2.png"});
+$('.p3').parallax({imageSrc:"images2/p3.png"});
+$('.p4').parallax({imageSrc:"images2/p4.png"});
+$('.p5').parallax({imageSrc:"images2/p5.png"});
+$('.p9').parallax({imageSrc:"images2/p9.png"});
+$('.p8').parallax({imageSrc:"images2/p8.png"});
